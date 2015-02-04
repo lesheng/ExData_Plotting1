6107c7d2c5fb64652275938c0c2b285e8644cc27
lesheng <konglesheng@gmail.com>
2015-02-04 21:54:52 +0000

    change codes

--- plot4.R
+++ plot4.R
@@ -5,10 +5,12 @@
 # download.file(fileUrl,destfile="household_power_consumption.zip")
 # unzip("household_power_consumption.zip", exdir=".")
 
+library(dplyr)
+
 # read the data
 power <- read.table("household_power_consumption.txt", header=TRUE, sep=";",colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings="?")
 
-# only sue the data from the dates 2007-02-01 and 2007-02-02
+# only use the data from the dates 2007-02-01 and 2007-02-02
 power_subset <- filter(power,Date %in% c("1/2/2007","2/2/2007"))
 
 # add a new column "dateTime".
